e67dac98811ba80f9176c192d6bb6d8e94214d88
Arya P P <aryapp2019@gmail.com>
2025-08-18 22:55:38 +0530

    Third commit

---
+++ Part1_TheJavaScriptLanguage/JavaScript_Fundamentals/2.13_Loops/task7.js
@@ -0,0 +1,16 @@
+let n = 10;
+
+for (let i = 2; i < n; i++) {
+    let flag = true;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = false; 
+            break;         
+        }
+    }
+
+    if (flag) {
+        alert(i);  
+    }
+}
