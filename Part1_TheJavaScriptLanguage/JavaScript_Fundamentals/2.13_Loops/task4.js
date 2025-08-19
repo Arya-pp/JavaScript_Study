a83c0305a74103755ac4793b80ae7505d7f722c8
Arya-pp <aryapp2019@gmail.com>
2025-08-19 20:02:06 +0530

    code quality

--- Part1_TheJavaScriptLanguage/JavaScript_Fundamentals/2.13_Loops/task4.js
+++ Part1_TheJavaScriptLanguage/JavaScript_Fundamentals/2.13_Loops/task4.js
@@ -1,3 +1,8 @@
-for(let i = 2; i <=10; i++){
-    if(i % 2 == 0) alert( i );
+for (let i = 2; i <= n; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue;
+  }
+
+  alert( i );
 }
