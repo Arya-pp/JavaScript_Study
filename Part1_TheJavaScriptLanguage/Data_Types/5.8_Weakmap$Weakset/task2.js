d21883415b2e07cbdcd3b72e494b6b95db1c275f
Arya-pp <aryapp2019@gmail.com>
2025-08-26 17:04:17 +0530

    destructuring

---
+++ Part1_TheJavaScriptLanguage/Data_Types/5.8_Weakmap$Weakset/task2.js
@@ -0,0 +1,13 @@
+let messages = [
+  {text: "Hello", from: "John"},
+  {text: "How goes?", from: "John"},
+  {text: "See you soon", from: "Alice"}
+];
+
+let readMap = new WeakMap();
+
+readMap.set(messages[0], new Date(2017, 1, 1));
+
+let readDate = readMap.get(messages[0]);
+
+console.log(readDate);
