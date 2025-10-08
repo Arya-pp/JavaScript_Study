949788b78431dfe86b8c96e8e8f99c7eebfb7011
Arya-pp <aryapp2019@gmail.com>
2025-10-08 17:26:13 +0530

    videoplayer completed

---
+++ test/prmg1.js
@@ -0,0 +1,28 @@
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// arr.forEach(element => {
+//     if (element > 5) {
+//         console.log(element);
+//     }
+// });
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
+// arr.forEach(element => {
+//     sum += element;
+// });
+// console.log(sum);
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let count = 0;
+// arr.forEach(element => {
+//     if (element % 3 == 0) {
+//         count++;
+//     }
+// });
+// console.log(count);
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+arr.forEach(element => {
+    console.log(element * 2);
+});
+
